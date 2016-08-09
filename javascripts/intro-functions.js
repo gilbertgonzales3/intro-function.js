ab88d9fdba745eb519656bfab0290f799be1a41d
GILBERT GONZALES <gilbertgonzales@GILBERTs-MacBook-Pro.local>
2016-08-09 17:43:56 -0500

    update to assignment

---
+++ javascripts/intro-functions.js
@@ -0,0 +1,148 @@
+var myObject = {
+  superPower: "strength",
+  anotherProperty: "strong",
+
+};
+console.log(myObject);
+/**
+ * PART 0
+ *
+ * Define a function max() that takes two numbers as arguments
+ * and returns the largest of them. Use the if-then-else construct available in Javascript.
+ */
+
+function max(a, b){
+  if (a >b){
+    return a;
+  }else{
+   return b;
+  }
+}
+
+console.assert(max(1,3) === 3);
+console.assert(max(0,3) === 3);
+console.assert(max(10,3) === 10);
+console.assert(max(-1,-3) === -1);
+console.assert(max("aaa",0) === 0);
+console.assert(isNaN(max("aaa","bbb")));
+
+/**
+ * PART 1
+ *
+ * Define a function maxOfThree() that takes three
+ * numbers as arguments and returns the largest of them.
+ */
+
+function maxOfThree(a, b, c){
+  if (a >b && a >c){
+    return a;
+  }else if (b > a && b>c){
+    return b;}
+   else{
+     return c;
+   }
+}
+
+console.assert(maxOfThree(1,3,2) === 3);
+console.assert(maxOfThree(0,3,-1) === 3);
+console.assert(maxOfThree(10,3,50) === 50);
+console.assert(maxOfThree(-1,-3,-10) === -1);
+console.assert(maxOfThree("aaa",0,1) === 1);
+console.assert(isNaN(maxOfThree("aaa","bbb","ccc")));
+
+/**
+ * PART 2
+ *
+ * Write a function isVowel() that takes a character (i.e. a string of length 1)
+ * and returns true if it is a vowel, false otherwise.
+ */
+
+function isVowel(char){
+  var vowel= ["a","e","i","o","u","A","E","I","O","U"];
+  //take char argument and compair to the letters in our vowel array
+  for (var i = 0; i < vowel.length; i++) {
+    if (char === vowel[i]) {
+      return true;
+    }
+  }
+    return false;
+
+}
+
+
+
+console.assert(isVowel(0) === false);
+console.assert(isVowel("B") === false);
+console.assert(isVowel("b") === false);
+console.assert(isVowel("a") === true);
+console.assert(isVowel("E") === true);
+
+/**
+ * PART 3
+ *
+ * Write a function rovarspraket() that will translate
+ * a text into a "rövarspråket". That is, double every
+ * consonant and place an occurrence of "o" in between.
+ *
+ * For example, rovarspraket("this is fun") should
+ * return the string "tothohisos isos fofunon".
+
+ */
+
+function rovarspraket(text){
+  var concatString = "";
+  for (var i = 0; i < text.length; i++) {
+    var currentletter = text[i];
+
+     if(!isVowel(currentletter)) {
+      concatString += currentletter + "o" + currentletter
+     }else {
+      concatString += text[i];
+    }
+  }
+
+  if (concatString === null){
+    return "0";
+  } else {
+    return concatString;
+  }
+
+}
+
+console.assert(rovarspraket("a") === "a")
+console.assert(rovarspraket("b") === "bob")
+console.assert(rovarspraket("cat") === "cocatot")
+console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
+console.assert(rovarspraket(0) === "0")
+
+/**
+ * Part 4
+ *
+ * Define a function reverse() that computes
+ * the reversal of a string. For example,
+ * reverse("skoob") should return the
+ * string "books".
+ */
+
+function reverse(str){
+    // YOUR CODE HERE
+}
+
+console.assert(reverse("books") === "skoob")
+console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+
+/**
+ * Part 5
+ *
+ * Write a function findLongestWord() that takes an
+ * string returns the first, longest word in the array.
+ *
+ * i.e. findLongestWord("book dogs") should return "book"
+ */
+
+function findLongestWord(sentence){
+    // YOUR CODE HERE
+}
+
+console.assert(findLongestWord("book dogs") === "book")
+console.assert(findLongestWord("don't mess with Texas") === "Texas")
